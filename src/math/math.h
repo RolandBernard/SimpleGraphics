87a78293839acaad209d869a664835ac9ad3f6a9
Roland Bernard <18496639+RolandBernard@users.noreply.github.com>
2019-01-04 13:20:00 +0100

    added math headers

---
+++ src/math/math.h
@@ -0,0 +1,16 @@
+#ifndef __MATH_H__
+#define __MATH_H__
+
+#include "scalar.h"
+
+#include "vec2.h"
+#include "vec3.h"
+#include "vec4.h"
+
+#include "mat2.h"
+#include "mat3.h"
+#include "mat4.h"
+
+#include "transform.h"
+
+#endif
